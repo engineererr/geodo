795a3388958476eb25c339c3842f84b3fa42485e
Kai Boschung <kai.boschung@garaio.com>
2018-04-29 17:26:39 +0200

    * removed .vscode/chrome * added calculation of batteryLevel

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import './App.css';
 
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import 'bootstrap/dist/css/bootstrap.css';
-
+// tslint:disable-next-line
 import axios from 'axios';
 import { Button } from 'react-bootstrap';
+import { Chart } from 'react-google-charts';
 
 import { AsyncTypeahead } from 'react-bootstrap-typeahead';
 
@@ -17,17 +18,76 @@ interface IAppState {
   isLoading: boolean;
   options: string[];
   flexItemsCount: number;
+  dataArray: object[];
 }
+
+// const dummyData = [['Zeit', 'Produktion', 'Verbrauch', 'Batterie'],
+// ['Jan', 165, 938, 0.4],
+// ['Feb', 135, 1120, 0.2],
+// ['Mar', 157, 1167, 0.7],
+// ['Apr', 139, 1110, 0.4],
+// ['Mai', 136, 691, 0.6],
+// ['Jun', 136, 1500, 0.6],
+// ['Jul', 157, 691, 0.9],
+// ['Aug', 136, 100, 2.3],
+// ['Sep', 800, 800, 4],
+// ['Oct', 136, 1300, 0.6],
+// ['Nov', 10, 2000, 1.6],
+// ['Dez', 136, 691, 0.6]];
+
+const dataSet = [
+  { time: 0, production: 10, consumtion: 3, batteryLevel: 0, totalEnergyOverflow: 0, kWhBought: 0 },
+  { time: 1, production: 10, consumtion: 20, batteryLevel: 0, totalEnergyOverflow: 0, kWhBought: 0 },
+  { time: 2, production: 10, consumtion: 3, batteryLevel: 0, totalEnergyOverflow: 0, kWhBought: 0 },
+  { time: 3, production: 10, consumtion: 3, batteryLevel: 0, totalEnergyOverflow: 0, kWhBought: 0 },
+  { time: 4, production: 10, consumtion: 3, batteryLevel: 0, totalEnergyOverflow: 0, kWhBought: 0 },
+];
 
 const apiEndpoint: string = "https://api3.geo.admin.ch/1804191145/rest/services/ech/SearchServer?sr=2056&lang=en&type=featuresearch&features=ch.bfs.gebaeude_wohnungs_register&timeEnabled=false&timeStamps=&searchText="
 class App extends React.Component<{}, IAppState> {
   constructor(props: any) {
     super(props);
     this.state = {
+      dataArray: [],
       flexItemsCount: 1,
       isLoading: false,
       options: [],
     };
+
+  }
+
+  public componentDidMount() {
+    this.calculate()
+  }
+  public calculateBatteryLevel(element, previousBatteryLevel) {
+    element.batteryLevel = previousBatteryLevel;
+    const totalEnergyOverflow: number = element.production - element.consumtion;
+    if (totalEnergyOverflow >= 0) {
+      // overflow positive, energy is saved to battery
+      element.batteryLevel += totalEnergyOverflow;
+    } else {
+      // overflow negative, energy from battery or Kraftwerk is used      
+      if (Math.abs(totalEnergyOverflow) < element.batteryLevel) {
+        element.batteryLevel -= totalEnergyOverflow;
+      } else {
+        const diff = Math.abs(totalEnergyOverflow) - element.batteryLevel;
+        element.batteryLevel = 0;
+        element.kWhBought = diff;
+      }
+    }
+    return element;
+  }
+  public calculate() {
+    let previousBatteryLevel = 0;
+    const resultingDataSet: any[][] = [];
+    resultingDataSet.push(['Zeit', 'Produktion', 'Verbrauch', 'Batterie']);
+    dataSet.forEach(element => {
+      element = this.calculateBatteryLevel(element, previousBatteryLevel);
+      resultingDataSet.push([element.time, element.production, element.consumtion, element.batteryLevel]);
+      previousBatteryLevel = element.batteryLevel;
+    });
+
+    this.setState({ dataArray: resultingDataSet });
   }
 
   public render() {
@@ -61,6 +121,15 @@ class App extends React.Component<{}, IAppState> {
         <div className="Flex-container">
           {this.renderSolarPanels()}
         </div>
+        <Chart chartType={"ComboChart"}
+          data={this.state.dataArray}
+          width="100%" legend_toggle={true} options={{
+            hAxis: { title: 'Month' },
+            series: { 2: { type: 'line', targetAxisIndex: 1 } },
+            seriesType: 'area',
+            title: 'Verbrauchsprofil',
+            vAxis: { title: 'Batterieladung' },
+          }} />
       </div>
     );
   }
@@ -78,7 +147,7 @@ class App extends React.Component<{}, IAppState> {
   }
   private renderSolarPanels = () => {
     // tslint:disable-next-line
-    let result = [];
+    let result: any = [];
     for (let i = 0; i < this.state.flexItemsCount; i++) {
       result.push(<div key={i} className="Flex-item">1qm</div>);
     }
